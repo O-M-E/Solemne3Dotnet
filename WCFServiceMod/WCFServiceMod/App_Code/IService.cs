5054307b0f42d895f43e53436e18d90c2f14c573
Dafs26 <dafz260@gmail.com>
2020-07-22 23:36:06 -0400

    v0.1

--- WCFServiceMod/WCFServiceMod/App_Code/IService.cs
+++ WCFServiceMod/WCFServiceMod/App_Code/IService.cs
@@ -12,32 +12,9 @@ public interface IService
 {
 
 	[OperationContract]
-	string GetData(int value);
-
-	[OperationContract]
-	CompositeType GetDataUsingDataContract(CompositeType composite);
-
+	int ObtenerModulo(int a, int b);
 	// TODO: agregue aquí sus operaciones de servicio
 }
 
 // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
-[DataContract]
-public class CompositeType
-{
-	bool boolValue = true;
-	string stringValue = "Hello ";
 
-	[DataMember]
-	public bool BoolValue
-	{
-		get { return boolValue; }
-		set { boolValue = value; }
-	}
-
-	[DataMember]
-	public string StringValue
-	{
-		get { return stringValue; }
-		set { stringValue = value; }
-	}
-}
--- WCFServiceMod/WCFServiceMod/App_Code/Service.cs
+++ WCFServiceMod/WCFServiceMod/App_Code/Service.cs
@@ -9,21 +9,8 @@ using System.Text;
 // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
 public class Service : IService
 {
-	public string GetData(int value)
-	{
-		return string.Format("You entered: {0}", value);
-	}
-
-	public CompositeType GetDataUsingDataContract(CompositeType composite)
-	{
-		if (composite == null)
-		{
-			throw new ArgumentNullException("composite");
-		}
-		if (composite.BoolValue)
-		{
-			composite.StringValue += "Suffix";
-		}
-		return composite;
-	}
+    public int ObtenerModulo(int a, int b)
+    {
+        return a % b;
+    }
 }
